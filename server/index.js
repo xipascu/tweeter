56786ffcc7bc76fa9c1173a6c5077d2ce99e2c12
Ilinca Pascu <xipascu@gmail.com>
2017-10-16 18:27:24 -0700

    tweeter nav bar.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
-const app           = express();
+const app   = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
