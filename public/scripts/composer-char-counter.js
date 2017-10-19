cf365b43fba70ed334abf33f66af135bb032efc4
Ilinca Pascu <xipascu@gmail.com>
2017-10-19 14:00:38 -0700

    Updated character counter.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.new-tweet form').on('keypress', 'textarea' , function() {
+    //console.log($(this), "heyo"); 
+    var countUpdate = $(this).parent().find('.counter');
+    var charLeft= 140 - $(this).val().length;
+    // console.log(charLeft);
+    countUpdate.text(charLeft);
+    // $(".counter").css()
+    var colorUpdate = (charLeft < 0) ? "#ff0000" : "#000000";
+    countUpdate.css({
+      color: colorUpdate
+    }).text(charLeft);
+  }); 
+});
+
